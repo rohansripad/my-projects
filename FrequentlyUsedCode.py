d4a9b0ffdadd1e9e587014b357822751b70486d9
rohansripad <rohansripad@users.noreply.github.com>
2017-10-09 14:01:58 -0700

    Update FrequentlyUsedCode.py

--- FrequentlyUsedCode.py
+++ FrequentlyUsedCode.py
@@ -3,6 +3,9 @@ import pandas as pd
 writer = pd.ExcelWriter('pandas_simple.xlsx')
 df.to_excel(writer, sheet_name='Sheet1')
 writer.save()
+-----------------------------------------
+#dataframe to CSV
+df.to_csv(file_name, sep='\t')
 ----------------------------------------------------------------------------------------------
 #Calculate time to run a code
 %timeit function

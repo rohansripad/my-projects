8ff08f003de0d7b4b7bd3f2f97c12549a49fb683
rohansripad <rohansripad@users.noreply.github.com>
2017-11-08 20:19:54 -0800

    Update FrequentlyUsedCode.py

--- FrequentlyUsedCode.py
+++ FrequentlyUsedCode.py
@@ -10,3 +10,15 @@ df.to_csv(file_name)
 #Calculate time to run a code
 %timeit function
 ----------------------------------------------------------------------------------------------
+#json to dataframe python
+#https://stackoverflow.com/questions/21104592/json-to-pandas-dataframe
+import json
+from pandas.io.json import json_normalize
+parsed_json = json.loads(json_string)
+json_normalize(parsed_json['results'])
+----------------------------------------------------------------------------------------------
+#fetch data from url
+import urllib2
+response = urllib2.urlopen('http://python.org/')
+html = response.read()
+----------------------------------------------------------------------------------------------

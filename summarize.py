040fa972cda6009663a608641f67b94c99a2c674
rohansripad <rohansripad@users.noreply.github.com>
2017-12-16 18:56:08 -0800

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -1,8 +1,31 @@
+import urllib2
 import nltk
+import sys
 from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 from __future__ import division
-f = open("/Users/rattravanam/Desktop/Sample_text.txt","r")
-text = f.read()
+from bs4 import BeautifulSoup
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+url = "http://www.latimes.com/local/california/la-me-lopez-commute-cherry-20171216-story.html"
+html = urllib2.urlopen(url).read()
+soup = BeautifulSoup(html,"lxml")
+
+# kill all script and style elements
+for script in soup(["script", "style"]):
+    script.extract()    # rip it out
+
+# get text
+text = soup.body.get_text(separator=' ')
+# break into lines and remove leading and trailing space on each
+lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+text = '\n'.join(chunk for chunk in chunks if chunk)
+
 text = text.decode('unicode_escape').encode('ascii','ignore')
 
 def trim_multi_newline(samp):
@@ -26,7 +49,7 @@ def summarize(text):
         sent_tokenize_list = sent_tokenize(i)
         if len(sent_tokenize_list)>1:
             summary.append("* "+sent_tokenize_list[0]+" "+sent_tokenize_list[len(sent_tokenize_list)-1])
-        else:
+        elif len(word_tokenize(i))>10:
             summary.append("* "+sent_tokenize_list[0])
     return summary
 

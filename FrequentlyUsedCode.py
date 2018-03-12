238ebaed3166d94754f629890ed03dad4ea69c78
rohansripad <rohansripad@users.noreply.github.com>
2018-03-12 15:41:17 -0700

    Update FrequentlyUsedCode.py

--- FrequentlyUsedCode.py
+++ FrequentlyUsedCode.py
@@ -3,7 +3,7 @@ import pandas as pd
 writer = pd.ExcelWriter('pandas_simple.xlsx')
 df.to_excel(writer, sheet_name='Sheet1')
 writer.save()
------------------------------------------
+#-----------------------------------------
 #dataframe to CSV
 df.to_csv(file_name)
 ----------------------------------------------------------------------------------------------
